03cbf74f6d6caacff3716b352902c37f98be7738
ktn9 <khenghau.tan@3ds.com>
2020-02-19 22:03:40 +0800

    implement upcoming & now showing movies

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -11,78 +11,91 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   var movies;
   var upcoming;
+  var nowShowing;
+  var i;
   Color mainColor = const Color(0xff3C3261);
 
-  void getData() async {
-    var data = await getJson();
-
+  void getPopularData() async {
+    var data = await getPopularJson();
     setState(() {
       movies = data['results'];
     });
   }
 
-  void getupcoming() async {
-    var data = await getUpComing();
-
+  void getUpComingData() async {
+    var data = await getUpComingJson();
+   
     setState(() {
       upcoming = data['results'];
     });
   }
-  
+
+  void getNowShowingData() async {
+    var data = await getNowShowingJson();
+   
+    setState(() {
+      nowShowing = data['results'];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    getData();
-    getupcoming();
+    getPopularData();
+    getUpComingData();
+    getNowShowingData();
 
-    return new Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
-      appBar: new AppBar(
+      appBar: AppBar(
         elevation: 0.3,
         centerTitle: true,
-        backgroundColor: Colors.white,
-        leading: new Icon(
+        backgroundColor: Color(0xff242248),
+       // backgroundColor: Colors.white,
+        leading: Icon(
           Icons.menu,
-          color: mainColor,
+          color: Color(0xff8267db),
         ),
-        title: new Text(
+        title: Text(
           'Movies',
-          style: new TextStyle(
-              color: mainColor,
+          style: TextStyle(
+              color: Colors.white,
               fontFamily: 'Arvo',
               fontWeight: FontWeight.bold),
         ),
         actions: <Widget>[
-          new Icon(
+           Icon(
             Icons.search,
-            color: mainColor,
+            color: Color(0xff8267db),
           )
         ],
       ),
       body: SingleChildScrollView(
         child: Container(
-          child: new Padding(
+          color: Colors.black,
+          child:  Padding(
             padding: const EdgeInsets.all(16.0),
-            child: new Column(
+            child:  Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                new MovieTitle(mainColor),
-                new Container(
-                  height: 250,
-                  child: new ListView.builder(
+                 MovieTitle(mainColor,'Popular'),
+                 Container(
+                  height: 350,
+                  child:  ListView.builder(
                       shrinkWrap: true,
                       scrollDirection: Axis.horizontal,
                       itemCount: movies == null ? 0 : movies.length,
                       itemBuilder: (context, i) {
-                        return new Column(
+                        return  Column(
                           children: <Widget>[
-                            new Container(
+                             Container(
                               //empty space between images
                               width: 250,
-                              height: 240,
-                              color: Colors.red,
-                              child: new FlatButton(
-                                child: new MovieCell(movies, i),
+                              height: 335,
+                              // color: Colors.red,
+                              child: Column(
+                                children: <Widget>[
+                                  FlatButton(
+                                child:  MovieCell(movies, i),
                                 padding: const EdgeInsets.all(0.0),
                                 onPressed: () {
                                   // Navigator.push(context,
@@ -92,28 +105,42 @@ class _MyHomePageState extends State<MyHomePage> {
                                 },
                                 //color: Colors.white,
                               ),
+                              Container(
+                                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 0),
+                                child: Text(movies[i]['title'],
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold
+                                ),),
+                              ),
+                                ]
+                              )
                             ),
+                           
                           ],
                         );
                       }),
                 ),
-                new MovieTitle(mainColor),
-                new Container(
-                  height: 250,
-                  child: new ListView.builder(
+                 MovieTitle(mainColor,'Upcoming'),
+                 Container(
+                  height: 350,
+                  child:  ListView.builder(
                       shrinkWrap: true,
                       scrollDirection: Axis.horizontal,
                       itemCount: upcoming == null ? 0 : movies.length,
                       itemBuilder: (context, i) {
-                        return new Column(
+                        return  Column(
                           children: <Widget>[
-                            new Container(
+                             Container(
                               //empty space between images
                               width: 250,
-                              height: 240,
-                              color: Colors.blue,
-                              child: new FlatButton(
-                                child: new MovieCell(upcoming, i),
+                              height: 335,
+                              // color: Colors.blue,
+                              child: Column(
+                                children: <Widget>[
+                                  FlatButton(
+                                child:  MovieCell(upcoming, i),
                                 padding: const EdgeInsets.all(0.0),
                                 onPressed: () {
                                   // Navigator.push(context,
@@ -123,28 +150,41 @@ class _MyHomePageState extends State<MyHomePage> {
                                 },
                                 //color: Colors.white,
                               ),
+                              Container(
+                                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 0),
+                                child: Text(upcoming[i]['title'],
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold
+                                ),),
+                              ),
+                                ]
+                              ),
                             ),
                           ],
                         );
                       }),
                 ),
-                new MovieTitle(mainColor),
-                new Container(
-                  height: 250,
-                  child: new ListView.builder(
+                 MovieTitle(mainColor,'Now Playing'),
+                 Container(
+                  height: 350,
+                  child:  ListView.builder(
                       shrinkWrap: true,
                       scrollDirection: Axis.horizontal,
                       itemCount: movies == null ? 0 : movies.length,
                       itemBuilder: (context, i) {
-                        return new Column(
+                        return  Column(
                           children: <Widget>[
-                            new Container(
+                             Container(
                               //empty space between images
                               width: 250,
-                              height: 240,
-                              color: Colors.greenAccent,
-                              child: new FlatButton(
-                                child: new MovieCell(movies, i),
+                              height: 335,
+                              // color: Colors.greenAccent,
+                              child: Column(
+                                children: <Widget>[
+                                  FlatButton(
+                                child:  MovieCell(nowShowing, i),
                                 padding: const EdgeInsets.all(0.0),
                                 onPressed: () {
                                   // Navigator.push(context,
@@ -153,6 +193,17 @@ class _MyHomePageState extends State<MyHomePage> {
                                   // }));
                                 },
                                 //color: Colors.white,
+                              ),
+                              Container(
+                                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 0),
+                                child: Text(nowShowing[i]['title'],
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold
+                                ),),
+                              ),
+                                ]
                               ),
                             ),
                           ],
@@ -168,16 +219,23 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-Future<Map> getJson() async {
+Future<Map> getPopularJson() async {
   var apiKey = '6a53377c0ee23811945a9e71a993601f';
   var url = 'https://api.themoviedb.org/3/movie/popular?api_key=$apiKey&language=en-US&page=1';
   var response = await http.get(url);
   return json.decode(response.body);
 }
 
-Future<Map> getUpComing() async{
+Future<Map> getUpComingJson() async{
   var apiKey = '6a53377c0ee23811945a9e71a993601f';
   var url = 'https://api.themoviedb.org/3/movie/upcoming?api_key=$apiKey&language=en-US&page=1';
+  var response = await http.get(url);
+  return json.decode(response.body);
+}
+
+Future<Map> getNowShowingJson() async{
+  var apiKey = '6a53377c0ee23811945a9e71a993601f';
+  var url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=$apiKey&language=en-US&page=1';
   var response = await http.get(url);
   return json.decode(response.body);
 }
@@ -185,17 +243,18 @@ Future<Map> getUpComing() async{
 
 class MovieTitle extends StatelessWidget {
   final Color mainColor;
+  final title;
 
-  MovieTitle(this.mainColor);
+  MovieTitle(this.mainColor, this.title);
 
   @override
   Widget build(BuildContext context) {
-    return new Padding(
+    return  Padding(
       padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
-      child: new Text(
-        'Popular',
-        style: new TextStyle(
-            fontSize: 20.0,
+      child:  Text(
+        title,
+        style:  TextStyle(
+            fontSize: 25.0,
             color: mainColor,
             fontWeight: FontWeight.bold,
             fontFamily: 'Arvo'),
@@ -214,71 +273,37 @@ class MovieCell extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new Column(
+    return  Column(
       children: <Widget>[
-        new Row(
+         Row(
           children: <Widget>[
-            new Padding(
+             Padding(
               padding: const EdgeInsets.all(0.0),
-              child: new Container(
+              child:  Container(
                 margin: const EdgeInsets.all(16.0),
-//                                child: new Image.network(image_url+movies[i]['poster_path'],width: 100.0,height: 100.0),
-                child: new Container(
+                child:  Container(
                   //image container size
                   width: 200.0,
                   height: 200.0,
                 ),
-                decoration: new BoxDecoration(
-                  borderRadius: new BorderRadius.circular(10.0),
+                decoration:  BoxDecoration(
+                  borderRadius:  BorderRadius.circular(10.0),
                   color: Colors.grey,
-                  image: new DecorationImage(
-                      image: new NetworkImage(
+                  image:  DecorationImage(
+                      image:  NetworkImage(
                           imageUrl + movies[i]['poster_path']),
                       fit: BoxFit.cover),
                   boxShadow: [
-                    new BoxShadow(
+                     BoxShadow(
                         color: mainColor,
                         blurRadius: 5.0,
-                        offset: new Offset(2.0, 5.0))
+                        offset:  Offset(2.0, 5.0))
                   ],
                 ),
               ),
             ),
-            // new Expanded(
-            //     child: new Container(
-            //   color: Colors.blue,
-            //   margin: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 0.0),
-            //   child: new Column(
-            //     children: [
-            //       // new Text(
-            //       //   movies[i]['title'],
-            //       //   style: new TextStyle(
-            //       //       fontSize: 20.0,
-            //       //       fontFamily: 'Arvo',
-            //       //       fontWeight: FontWeight.bold,
-            //       //       color: mainColor),
-            //       // ),
-            //       new Padding(padding: const EdgeInsets.all(2.0)),
-            //       // new Text(
-            //       //   movies[i]['overview'],
-            //       //   maxLines: 1,
-            //       //   style: new TextStyle(
-            //       //       color: const Color(0xff8785A4), fontFamily: 'Arvo'),
-            //       // )
-            //     ],
-            //     crossAxisAlignment: CrossAxisAlignment.start,
-            //   ),
-            // )),
           ],
         ),
-        // new Container(
-        //   width: 100.0,
-        //   height: 0.5,
-
-        //   //color: const Color(0xD2D2E1ff),
-        //   color: Colors.blue,
-        //   margin: const EdgeInsets.all(16.0),
-        // )
       ],
     );
   }
